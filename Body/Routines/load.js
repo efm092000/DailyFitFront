ce2bcccdc30cf2a569dfe4a619c0c031887bb51f
naixin9393 <naicinchen@gmail.com>
2024-03-15 23:43:24 +0000

    feat: add creating new WeeklyPlan

--- Body/Routines/load.js
+++ Body/Routines/load.js
@@ -1,6 +1,2 @@
-import { loadTemplate, replaceBody} from "../../actions.js";
 
-export async function loadRoutinesBody(user) {
-    let routines = await loadTemplate('/Body/Routines/Routines.html');
-    replaceBody(routines);
-}
+
--- Body/weekly/load.js
+++ Body/weekly/load.js
@@ -11,6 +11,18 @@ export async function loadWeekly(user) {
 	loadAddRoutineButton(weeklyPlanContainer);
 	await loadWeeklyPlan(user, weeklyPlanContainer);
 	await loadDaysOfWeekTemplate(weeklyPlanContainer);
+	loadTitleAction(user, weeklyPlanContainer);
+}
+
+function loadTitleAction(user, weeklyPlanContainer) {
+	weeklyPlanContainer.querySelector('.weekly-title').addEventListener('change', async function () {
+		let selector = weeklyPlanContainer.querySelector('.weekly-plans-selector');
+		let wid = selector.value;
+		await fetch(`http://localhost:8080/api/weekly/${wid}?name=${this.value}`, {
+			method: 'PUT'
+		});
+		selector.options[selector.selectedIndex].label = this.value;
+	});
 }
 
 export function reloadContent(weeklyPlanContainer, wid) {
@@ -27,7 +39,7 @@ function loadAddRoutineButton(weeklyPlanContainer) {
 
 async function loadWeeklyPlan(user, weeklyPlanContainer) {
 	let weeklyPlansJson = await fetch(`http://localhost:8080/api/user/${user.email}/weeklies`).then(response => response.json());
-	let weeklyPlansSelector = loadWeeklyPlanSelector(weeklyPlanContainer);
+	let weeklyPlansSelector = loadWeeklyPlanSelector(weeklyPlanContainer, user);
 	for (const weeklyPlan of weeklyPlansJson) {
 		let option = document.createElement('option');
 		option.label = weeklyPlan.name;
@@ -37,19 +49,54 @@ async function loadWeeklyPlan(user, weeklyPlanContainer) {
 	}
 }
 
-function loadWeeklyPlanSelector(weeklyPlanContainer) {
+function loadWeeklyPlanSelector(weeklyPlanContainer, user) {
 	let weeklyPlansSelector = weeklyPlanContainer.querySelector('.weekly-plans-selector');
+	weeklyPlansSelector.innerHTML = '';
 
+	let defaultOption = loadDefaultOption();
+	weeklyPlansSelector.appendChild(defaultOption);
+
+	let createOption = loadCreateOption();
+	weeklyPlansSelector.appendChild(createOption);
+
+	loadWeeklyPlanSelectorAction(weeklyPlanContainer, weeklyPlansSelector, user);
+	return weeklyPlansSelector;
+}
+
+async function loadWeeklyPlanSelectorAction(weeklyPlanContainer, weeklyPlansSelector, user) {
+	weeklyPlansSelector.addEventListener('change', async function () {
+		if (this.value === 'create') {
+			await createNewWeeklyPlan(weeklyPlanContainer, user);
+			return;
+		}
+		await loadRoutines(weeklyPlanContainer, this.value);
+	});
+}
+
+async function createNewWeeklyPlan(weeklyPlanContainer, user) {
+	let wid = await fetch(`http://localhost:8080/api/weekly/${user.email}?name=New_Weekly_Plan`, {
+		method: 'POST'
+	})
+	.then(response => response.json())
+	.then(json => json.wid);
+	await loadRoutines(weeklyPlanContainer, wid);
+	await loadWeeklyPlan(user, weeklyPlanContainer);
+	weeklyPlanContainer.querySelector('.weekly-plans-selector').value = wid;
+}
+
+function loadDefaultOption() {
 	let defaultOption = document.createElement('option');
 	defaultOption.label = 'Select a weekly plan';
 	defaultOption.disabled = true;
 	defaultOption.selected = true;
+	return defaultOption;
+}
 
-	weeklyPlansSelector.appendChild(defaultOption);
-	weeklyPlansSelector.addEventListener('change', function () {
-		loadRoutines(weeklyPlanContainer, this.value);
-	});
-	return weeklyPlansSelector;
+function loadCreateOption() {
+	let createWeeklyPlanOption = document.createElement('option');
+	createWeeklyPlanOption.label = 'Create a new weekly plan';
+	createWeeklyPlanOption.value = 'create';
+	return createWeeklyPlanOption;
 }
 
 async function loadDaysOfWeekTemplate(weekly) {
@@ -63,6 +110,7 @@ async function loadDaysOfWeekTemplate(weekly) {
 
 async function loadRoutines(weeklyPlanContainer, wid) {
 	let json = await fetch(`http://localhost:8080/api/weekly/${wid}/routines`).then(response => response.json());
+	weeklyPlanContainer.querySelector('.weekly-title').value = await fetch(`http://localhost:8080/api/weekly/${wid}`).then(response => response.json()).then(json => json.name);
 	clearTable(weeklyPlanContainer);
 	transformData(json);
 	for (const routine of json) {
