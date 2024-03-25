3ac1425528e85bd5ebad1a648a4da33ffaefe659
naixin <naixin.chen101@alu.ulpgc.es>
2024-03-25 01:34:22 +0000

    feat: add sign up logic

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { JsonPipe, NgIf } from "@angular/common";
-import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { RouterLink, RouterOutlet } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-sign-up',
-  standalone: true,
+	selector: 'app-sign-up',
+	standalone: true,
 	imports: [
 		JsonPipe,
 		NgIf,
@@ -13,37 +14,47 @@ import { RouterLink, RouterOutlet } from "@angular/router";
 		RouterOutlet,
 		RouterLink
 	],
-  templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+	templateUrl: './sign-up.component.html',
+	styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
 	question: string = 'Already have an account? Log in ';
 
-	constructor(private fb: FormBuilder) { }
+	constructor(private fb: FormBuilder, private http: HttpClient) { }
 
-	formSignUp = this.fb.group({
+	formSignUp: FormGroup = this.fb.group({
 		'email': ['', [Validators.required, Validators.email]],
+		'username': ['', [Validators.required]],
 		'password': ['', [Validators.required]],
 		'confirmPassword': ['', [Validators.required]]
 	})
 
-	get email() {
+	signUp(): void {
+		let signUpUrl = `http://localhost:8080/api/user/${this.email.value}?name=${this.username.value}&password=${this.password.value}`;
+		this.http.post(signUpUrl, {}, { responseType: 'text'})
+		.subscribe({
+			next: response => alert(response),
+			error: response => alert(response.error)
+		});
+	}
+
+	passwordsMatch(): boolean {
+		return this.password.value === this.confirmPassword.value;
+	}
+
+	get username(): FormControl {
+		return this.formSignUp.get('username') as FormControl;
+	}
+
+	get email(): FormControl {
 		return this.formSignUp.get('email') as FormControl;
 	}
 
-	get password() {
+	get password(): FormControl {
 		return this.formSignUp.get('password') as FormControl;
 	}
 
-	get confirmPassword() {
+	get confirmPassword(): FormControl {
 		return this.formSignUp.get('confirmPassword') as FormControl;
-	}
-
-	signUp() {
-		// TODO: Implement sign-up logic
-	}
-
-	passwordsMatch() {
-		return this.password.value === this.confirmPassword.value;
 	}
 }
