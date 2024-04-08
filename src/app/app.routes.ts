b068681a36c9ce937473d9a2de8e6f9245665ed7
ElenaFM <efm092000@gmail.com>
2024-04-08 11:46:01 +0100

    Refactorization, header angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PruebaComponent } from "./components/prueba/prueba.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'prueba', component:PruebaComponent
+  }
+];
