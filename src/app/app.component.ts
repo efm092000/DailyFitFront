750a227fe135b7473a8578b646afe5521406109f
Alejandro <apadoss03@gmail.com>
2024-04-08 19:46:04 +0100

    Changed app.config.ts file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {RoutineComponent} from "./components/routine/routine.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RoutineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
