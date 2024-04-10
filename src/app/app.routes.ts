5ebc7cf59e306e0fd0f4f914dab0e3252ec0fc6a
PabloSeguraLopez <pabseglop@gmail.com>
2024-04-10 09:48:39 +0100

    ExerciseDetails PopUp implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ExerciseSearchComponent} from "./components/exercise-search/exercise-search.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ExerciseSearchComponent}
+];
