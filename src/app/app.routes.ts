09acdc84bebe0aed189f65a9b722148cb8c3f1f7
DanielFabelo <dfabeloizquierdo@gmail.com>
2024-04-14 20:18:15 +0100

    Added exercise-search as lazy loading in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import {ExerciseSearchComponent} from "./components/exercise-search/exercise-search.component";
 
 export const routes: Routes = [
-  { path: 'search', component: ExerciseSearchComponent},
+  { path: 'home', loadComponent: () => import('./components/home-page/home-page.component').then(m => m.HomePageComponent)},
+  { path: 'search', loadComponent: () => import('./components/exercise-search/exercise-search.component').then(m => m.ExerciseSearchComponent)},
+  { path: 'details', loadComponent: () => import('./components/exercise-search/exercise-details/exercise-details.component').then(m => m.ExerciseDetailsComponent)},
   { path: 'routines', loadComponent: () => import('./components/routines/routines.component').then(m => m.RoutinesComponent)},
 	{ path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)},
 	{ path: 'sign-up', loadComponent: () => import('./components/sign-up/sign-up.component').then(m => m.SignUpComponent)},
+  { path: 'sidebar', loadComponent: () => import('./components/sidebar/sidebar.component').then(m => m.SidebarComponent)},
   { path: 'routine', loadComponent: () => import('./components/routine/routine.component').then (m => m.RoutineComponent)}
 ];
