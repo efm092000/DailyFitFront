d39da8c6978ae2c101048f94f0ac290c64fa65c9
ElenaFM <efm092000@gmail.com>
2024-04-19 11:11:21 +0100

    Solved problems in method onWeeklyPlanChange

--- src/app/components/weekly/weekly.component.ts
+++ src/app/components/weekly/weekly.component.ts
@@ -3,10 +3,10 @@ import { WeeklyService } from '../../services/weekly.service';
 import { Weekly } from '../../interfaces/weekly';
 import { UserRoutines} from '../../interfaces/user-routines.interface';
 import {NgForOf, NgIf} from "@angular/common";
-import {Observable} from "rxjs";
+
 import {FormsModule} from "@angular/forms";
 import {RoutinesService} from "../../services/routines.service";
-import {Routine} from "../../interfaces/routine.interface";
+
 
 @Component({
   selector: 'app-weekly',
@@ -20,70 +20,74 @@ import {Routine} from "../../interfaces/routine.interface";
   styleUrls: ['./weekly.component.css']
 })
 export class WeeklyComponent implements OnInit {
-  weeklyPlans: Weekly[] = [] ;
+  weeklyPlans: Weekly[] = [];
   routines: UserRoutines[] | undefined = [];
   selectedWeeklyPlanIndex: number = 0;
   editingMode: boolean = false;
   selectedWid: number = 0;
-  showPopup:  boolean = false;
+  showPopup: boolean = false;
   showInput: boolean = false;
   daysOfWeek: string[] = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-  newWeeklyName: string='New_Weekly_Plan';
+  newWeeklyName: string = 'New_Weekly_Plan';
 
-  constructor(private weeklyService: WeeklyService, private routinesService: RoutinesService) {}
+  constructor(private weeklyService: WeeklyService, private routinesService: RoutinesService) {
+  }
 
   ngOnInit(): void {
     this.loadWeeklyPlans();
-    this.onWeeklyPlanChange();
     this.loadRoutines()
   }
 
   loadWeeklyPlans(): void {
     this.weeklyService.getWeeklyPlans().subscribe((weeklyPlans: Weekly[]) => {
       this.weeklyPlans = weeklyPlans;
-    }, (error) =>{
+    }, (error) => {
       console.error('Error al obtener los weeklies:', error);
     });
   }
-  loadRoutines(): void{
+
+  loadRoutines(): void {
     this.routinesService.getUserRoutines().subscribe(
       {
         next: (routines: UserRoutines[] | undefined) => {
           this.routines = routines;
-        },
-        error: (error) => {
+        }, error: (error) => {
           console.log('Error al cargar las rutinas', error);
         }
       });
   }
-  loadRoutinesOfWeekly(){
 
-    this.weeklyService.getRoutinesOfWeeklyPlan(this.selectedWid).subscribe((routines: UserRoutines[])=>{
+  loadRoutinesOfWeekly() {
+    this.weeklyService.getRoutinesOfWeeklyPlan(this.selectedWid).subscribe((routines: UserRoutines[]) => {
       this.routines = routines;
-    }, (error) =>{
+    }, (error) => {
       console.error('Error al obtener las rutinas:', error);
     });
   }
+
   toggleEditMode(): void {
     this.editingMode = !this.editingMode;
   }
+
   togglePopup() {
-    if(!this.showInput){
-      this.showPopup =!this.showPopup;
+    if (!this.showInput) {
+      this.showPopup = !this.showPopup;
     }
   }
 
-  toggleInput(){
-    if(!this.showPopup){
-      this.showInput =!this.showInput
+  toggleInput() {
+    if (!this.showPopup) {
+      this.showInput = !this.showInput
     }
   }
+
   async addWeeklyPlan(name: string): Promise<void> {
     console.log("1");
     this.weeklyService.createNewWeeklyPlan(`123@gmail.com`, name).subscribe((weekly: Weekly) => {
-      this.loadWeeklyPlans();},
-      (error) =>{
-      console.error(`Error al añadir un weekly`, error);
+        this.loadWeeklyPlans();
+      },
+      (error) => {
+        console.error(`Error al añadir un weekly`, error);
       });
     this.toggleInput();
   }
@@ -93,17 +97,17 @@ export class WeeklyComponent implements OnInit {
     this.togglePopup()
   }
 
-  onWeeklyPlanChange(): void{
+  onWeeklyPlanChange(): void {
     const selectedWeeklyPlan = this.weeklyPlans[this.selectedWeeklyPlanIndex];
-    this.selectedWid = selectedWeeklyPlan.wid;
-    console.log("Selected wid:", this.selectedWid);
-    this.loadRoutinesOfWeekly();
+    if (selectedWeeklyPlan) {
+      this.selectedWid = selectedWeeklyPlan.wid;
+      console.log("Selected wid:", this.selectedWid);
+      this.loadRoutinesOfWeekly();
+    } else {
+      console.error("No se ha seleccionado ningún plan semanal.");
+    }
   }
 
 
-
-}
-function next(value: UserRoutines[] | undefined): void {
-    throw new Error('Function not implemented.');
 }
 
