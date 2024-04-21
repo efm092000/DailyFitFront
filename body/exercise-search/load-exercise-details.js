b46682a30325b130623bc479bcd880495b8cb40f
PabloSeguraLopez <pabseglop@gmail.com>
2024-04-21 18:53:55 +0100

    Non angular archives deleted

--- body/exercise-search/load-exercise-details.js
+++
@@ -1,7 +0,0 @@
-import {loadTemplate, replaceBody} from "../../actions.js";
-
-export async function loadExerciseDetailsPage(name) {
-    let exerciseDetailsPage = await loadTemplate('/body/exercise-search/exercise-details.html');
-    exerciseDetailsPage.getElementById("exercise-name").innerText = name;
-    replaceBody(exerciseDetailsPage);
-}
