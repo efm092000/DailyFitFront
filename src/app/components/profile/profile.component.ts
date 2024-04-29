991d60023a28369fc138c672db6289d8049af589
DanielFabelo <159779101+DanielFabelo@users.noreply.github.com>
2024-04-29 12:02:45 +0100

    Routines number calculator added

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {User} from "../../interfaces/user";
 import {NgOptimizedImage} from "@angular/common";
+import {RoutinesService} from "../../services/routines.service";
 
 @Component({
   selector: 'app-profile',
@@ -14,10 +15,14 @@ import {NgOptimizedImage} from "@angular/common";
 })
 export class ProfileComponent implements OnInit{
   user: User = {name:"", email:""};
-  constructor(private userService: UserService) { }
+  numRoutines: number = 0;
+  constructor(private userService: UserService, private routinesService: RoutinesService) { }
 
   ngOnInit(): void {
     this.user = this.userService.getLoggedInUser();
+    this.routinesService.getNumberOfRoutines().subscribe(
+      num => this.numRoutines = num
+    );
   }
 
   editProfile(): void {
