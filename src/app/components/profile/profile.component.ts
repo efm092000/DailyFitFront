5a555899c84f2818dae5c1de439d679a1aa31cf6
DanielFabelo <159779101+DanielFabelo@users.noreply.github.com>
2024-05-06 13:56:55 +0100

    Update: User information updated at real time

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,14 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {User} from "../../interfaces/user";
-import {NgOptimizedImage} from "@angular/common";
+import {NgIf, NgOptimizedImage} from "@angular/common";
 import {RoutinesService} from "../../services/routines.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    FormsModule,
+    NgIf
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
@@ -16,16 +19,34 @@ import {RoutinesService} from "../../services/routines.service";
 export class ProfileComponent implements OnInit{
   user: User = {name:"", email:""};
   numRoutines: number = 0;
+  newName: string = '';
+  isEditModalOpen: boolean = false;
   constructor(private userService: UserService, private routinesService: RoutinesService) { }
 
   ngOnInit(): void {
-    this.user = this.userService.getLoggedInUser();
+
+    //this.user = this.userService.getLoggedInUser();
+    this.userService.user$.subscribe(
+      user => this.user = user
+    );
     this.routinesService.getNumberOfRoutines().subscribe(
       num => this.numRoutines = num
     );
   }
+  openEditModal(): void {
+    this.newName = this.user.name; // Pre-fill the input with current name
+    this.isEditModalOpen = true;
+    console.log(this.isEditModalOpen);
+  }
 
-  editProfile(): void {
+  closeEditModal(): void {
+    this.isEditModalOpen = false; // Hide the modal
+  }
+
+  submitEditProfile(): void {
+    // Update user name
+    this.userService.updateName(this.user.email, this.newName);
+    this.closeEditModal();
   }
 }
 
