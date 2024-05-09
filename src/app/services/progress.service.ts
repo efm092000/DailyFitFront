e475929dfa929be7296c67722f0fa4c66a52d483
naixin <naixin.chen101@alu.ulpgc.es>
2024-05-09 21:57:22 +0000

    feat: show sets/reps/weight in graph

--- src/app/services/progress.service.ts
+++ src/app/services/progress.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Progress } from "../interfaces/progress";
 import {HttpClient} from "@angular/common/http";
 import {ExerciseDone} from "../interfaces/exercise-done";
 import {UserService} from "./user.service";
@@ -11,16 +10,6 @@ export class ProgressService {
   private apiUrl = "http://localhost:8080/api/progress/";
 
   constructor(private http: HttpClient, private userService: UserService) { }
-
-  async getProgressMock(userEmail: string, exerciseName: string): Promise<Progress> {
-    const response = await fetch(this.apiUrl + `get_done_exercises?email=${userEmail}&name=${exerciseName}`);
-    const data = await response.json();
-    console.log(data)
-    return {
-      exercise: exerciseName,
-      data: data
-    };
-  }
 
   async getProgressExerciseName(userEmail:string) {
     const request = fetch(this.apiUrl + `get_done_exercises_names?email=${userEmail}`);
