a66702547d1137c594ea8c17f780854ca6220476
Alejandro <apadoss03@gmail.com>
2024-05-13 20:52:44 +0100

    Added form validation to premium form.

--- src/app/components/subscription/subscription.component.ts
+++ src/app/components/subscription/subscription.component.ts
@@ -3,22 +3,40 @@ import {countries} from "./country-data";
 import {NgForOf, NgIf} from "@angular/common";
 import {User} from "../../interfaces/user";
 import {UserService} from "../../services/user.service";
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-subscription',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    ReactiveFormsModule
   ],
   templateUrl: './subscription.component.html',
   styleUrl: './subscription.component.css'
 })
 export class SubscriptionComponent implements OnInit{
   public countries: any = countries
+  formPremium = this.fb.group({
+      cardNumber: ['', [Validators.required,
+        Validators.pattern("(?<!\\d)\\d{16}(?!\\d)")]
+      ],
+      expireDate: ['', [Validators.required,
+        Validators.pattern("^(0[1-9]|1[0-2])\\/?([0-9]{2})$")]
+      ],
+      cvc: ['', [Validators.required,
+        Validators.pattern("(?<!\\d)\\d{3}(?!\\d)")]
+      ],
+      streetAddress: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      zipCode: ['', [Validators.required,
+        Validators.pattern("^\\d{5}(?:[-\\s]\\d{4})?$")]]
+    }
+  );
   user: User = {email:'',name:'',isPremium:false,isAdmin:false,profilePicture:''};
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
@@ -26,6 +44,28 @@ export class SubscriptionComponent implements OnInit{
       user => this.user = user
     );
   }
+
+  get cardNumber() {
+    return this.formPremium.get('cardNumber') as FormControl;
+  }
+  get expireDate() {
+    return this.formPremium.get('expireDate') as FormControl;
+  }
+  get cvc() {
+    return this.formPremium.get('cvc') as FormControl;
+  }
+
+  get streetAddress() {
+    return this.formPremium.get('streetAddress') as FormControl;
+  }
+
+  get city() {
+    return this.formPremium.get('city') as FormControl;
+  }
+  get zipCode() {
+    return this.formPremium.get('zipCode') as FormControl;
+  }
+
 
   buyPremium(){
     this.userService.getPremium(this.user.email)
