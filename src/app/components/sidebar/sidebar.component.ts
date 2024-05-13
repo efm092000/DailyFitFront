d00ca638c34028c7ce5a0c8eef250a7e61ec0a3f
Alejandro <apadoss03@gmail.com>
2024-05-13 11:42:36 +0100

    Solved error when admin field wasn't correctly detected.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {NgClass, NgIf} from "@angular/common";
 import {UserService} from "../../services/user.service";
+import {User} from "../../interfaces/user";
 
 @Component({
   selector: 'app-sidebar',
@@ -10,18 +11,25 @@ import {UserService} from "../../services/user.service";
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+  hideElements: boolean = false;
+  image: string = "assets/images/left-arrow.png";
+  isAdmin: boolean = false;
+  private user: User = {email:'',name:'',isPremium:false,isAdmin:false,profilePicture:''};
 
   constructor(private userService: UserService) {
   }
 
-  hideElements: boolean = false;
-  image: string = "assets/images/left-arrow.png";
-  isAdmin: boolean = this.userService.isUserAdmin();
+  ngOnInit() {
+    this.userService.user$.subscribe((user:User) => {this.user = user})
+  }
 
   hideSidebar() {
     this.hideElements = !this.hideElements;
-    console.log(this.isAdmin);
     this.image = this.hideElements ? "assets/images/right-arrow.png" : "assets/images/left-arrow.png";
+  }
+
+  checkAdmin() {
+    return this.user.isAdmin;
   }
 }
